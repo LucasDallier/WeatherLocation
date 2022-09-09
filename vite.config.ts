440824893bfb030a7f2a3fd82ee211d6c3b20244
lucas <l.dallier@xsensors.ai>
2022-09-09 16:20:38 -0300

    configure start deploy github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploying-vite-project-weather-location",
+  base: "/WeatherLocation",
   plugins: [react(), svgr()],
 });
